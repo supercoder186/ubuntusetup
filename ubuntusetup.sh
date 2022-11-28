5be96175152bb0259d4f10040e2bbfa05facdc09
Aadit Kolar <aadit.kolar@gmail.com>
2022-11-28 13:55:51 -0600

    Added opera ffmpeg fix and dock customizations

--- ubuntusetup.sh
+++ ubuntusetup.sh
@@ -16,6 +16,11 @@ echo Installing gnome tweaks
 sudo apt-get install gnome-tweaks git dkms build-essential -y
 echo Disabling mouse accelaration
 sudo apt install dconf-editor -y
+echo customizing dock
+gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
+gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 40
+gsettings set org.gnome.shell.extensions.dash-to-dock show-apps-at-top true
 gsettings set org.gnome.desktop.peripherals.mouse accel-profile 'flat'
 xinput --set-prop 10 "Coordinate Transformation Matrix" 1 0 0 0 1 0 0 0 1
 gsettings set org.gnome.desktop.peripherals.mouse speed 0
@@ -28,6 +33,11 @@ sudo apt-get install snapd -y
 wget "https://download.opera.com/download/get/?id=55747&location=415&nothanks=yes&sub=marine&utm_tryagain=yes" -O opera.deb
 sudo apt install ./opera.deb -y
 rm opera.deb
+echo fixing opera media codecs
+git clone https://github.com/xZero707/opera-fix-ffmpeg
+cd opera-fix-ffmpeg
+./opera-fix-ffmpeg.sh /usr/lib/x86_64-linux-gnu/opera/
+cd ..
 echo removing firefox
 sudo apt remove firefox -y
 sudo snap remove firefox
